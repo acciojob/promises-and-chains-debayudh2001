83a9208249e7074308ef4c31bcdc169df466d222
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-11 17:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let form = document.querySelector("#form")
+let name = document.querySelector("#name")
+let age = document.querySelector("#age")
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	if(name.value == "" && age.value == ""){
+		alert("Please enter valid details.")
+		return
+	}
+	let prom = new Promise((resolve, reject) => {
+	if(age.value > 18){
+		resolve(setTimeout(() => {
+			alert("Welcome, . You can vote.")
+		}, 4000))
+	}else{
+		resolve(setTimeout(() => {
+			alert("Oh sorry . You aren't old enough.")
+		}, 4000))
+	}
+    })
+})
+
